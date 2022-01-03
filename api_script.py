043f781aed1c272e268f705b5e7d9555bccb9a4f
sang <murdenh1999@gmail.com>
2022-01-03 19:29:37 +1100

    Final commit

---
+++ api_script.py
@@ -0,0 +1,17 @@
+import requests
+
+
+host = 'https://udacity-mlop-test.herokuapp.com'
+predict_uri = f"{host}/predict"
+
+item = {'age': 42, 'workclass': 'Private', 'fnlgt': 159449, 'education': 'Bachelors', 'education-num': 13, 'marital-status': 'Married-civ-spouse', 'occupation': 'Exec-managerial', 'relationship': 'Husband', 'race': 'White', 'sex': 'Male', 'capital-gain': 5178, 'capital-loss': 0, 'hours-per-week': 40, 'native-country': 'United-States'}
+    
+r = requests.post(predict_uri, json=item)
+
+
+assert r.status_code == 200
+assert r.json() == {"prediction": 1}
+
+print('Request body: ', item)
+print('Status code: ', r.status_code)
+print('Reponse: ', r.json())
